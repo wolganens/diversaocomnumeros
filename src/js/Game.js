cfabea3b7e274180b5722e38f446e1c7f3e32cd0
Wolgan Ens <ens.wolgan@gmail.com>
2018-07-07 21:13:20 -0300

    habilitar/desabilitar música

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
-
+import disabledMusic from '../imgs/somDesativadoAzul.png';
+import enabledMusic from '../imgs/somAtivadoAzul.png';
 import '../css/game.css'
 
 class GameInfo extends React.Component {
@@ -119,15 +120,51 @@ class GameQuestion extends React.Component {
   }
 }
 class GameActions extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      enabledMusic: false
+    }
+    this.disableMusic = this.disableMusic.bind(this);
+    this.enableMusic = this.enableMusic.bind(this);
+  }
+  disableMusic() {
+    this.setState({
+      enabledMusic: false
+    })
+    this.music.audioEl.pause()
+  }
+  enableMusic() {
+    this.setState({
+      enabledMusic: true
+    })
+    this.music.audioEl.play()
+  }
   render() {
     return (
-      <div>
+      <div id="game-actions">
+        {this.state.enabledMusic ? (
+          <button
+            id="enabled-sound"
+            className="clean"
+            onClick={this.disableMusic}>
+              <img className="img-responsive" src={enabledMusic} alt="Pausar música de fundo"/>
+          </button>
+        ) : (
+          <button
+            id="disabled-sound"
+            className="clean"
+            onClick={this.enableMusic}>
+              <img className="img-responsive" src={disabledMusic} alt="Pausar música de fundo"/>
+          </button>
+        )}
         <ReactAudioPlayer
-            src="/sons/musica.mp3"
+            src="/sons/Bubble_Bath.mp3"
             ref={(element) => { this.music = element; }}
             autoPlay={false}
             className="hidden"
             controls
+            loop={true}
           />
       </div>
     )
