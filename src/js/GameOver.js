dc4e1482b742f974b803daad1a70e44d48f45a4d
Wesley Ferreira de Ferreira <wferreira531@gmail.com>
2018-07-23 14:56:17 -0300

    Orca agora lê o texto da tela de fim de jogo(add tabindex)

--- src/js/GameOver.js
+++ src/js/GameOver.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default class GameOver extends React.Component {  
+export default class GameOver extends React.Component {
   render() {
     return (
       <section className="quadroJogo text-center text-board">
-        <div className="grand">Fim do Jogo!</div>
-        <div className="medium">Sua pontuação foi:</div>
-        <div className="grand text-center">{this.props.score}</div>
+        <div tabindex="1" className="grand">Fim do Jogo!</div>
+        <div tabindex="2" className="medium">Sua pontuação foi:</div>
+        <div tabindex="3" className="grand text-center">{this.props.score}</div>
         <button
           onClick={this.props.onStartGame}
           className="button">Jogar novamente
