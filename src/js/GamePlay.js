e31161b39ad7373e527d41dd310dfc2742f9842e
Wolgan Ens <ens.wolgan@gmail.com>
2018-08-07 23:36:38 -0300

    Fabrica de contas movido para Utils

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -4,12 +4,14 @@ import GameInfo from './GameInfo';
 import GameQuestion from './GameQuestion';
 import GameActions from './GameActions';
 import GameNav from './GameNav';
+import { CalcFactory } from './Utils';
 
 export default class GamePlay extends React.Component {
   constructor(props) {
     super(props)
     this.incorrectHandler = this.incorrectHandler.bind(this);
     this.correctHandler = this.correctHandler.bind(this);
+    this.factory = new CalcFactory();
   }
   incorrectHandler() {
     this.error.audioEl.play()
@@ -32,7 +34,7 @@ export default class GamePlay extends React.Component {
           <GameQuestion
             onCorrentAnswer={this.correctHandler}
             onIncorrectAnswer={this.incorrectHandler}
-            question={gerarNovaConta(this.props.score, this.props.level)}
+            question={this.factory.make(this.props.score, this.props.level)}
             isPaused={this.props.isPaused}
           />
           <GameActions/>
@@ -55,141 +57,4 @@ export default class GamePlay extends React.Component {
       </section>
     );
   }
-}
-function gerarNovaConta (pontos, dificuldade) {
-  let n1;
-  let opsig;
-  let op;
-  let n2;
-
-  let probabilidadeSoma;
-  let probabilidadeSubtracao;
-  let probabilidadeMultiplicacao;
-  let transportePermitido;
-  let n1Maximo;
-  let n2Maximo;
-
-  /* com base na dificuldade */
-  switch(dificuldade){
-    case 1:
-        probabilidadeSoma = 0.75;
-        probabilidadeSubtracao = 0.25;
-        probabilidadeMultiplicacao = 0.0;
-        transportePermitido = false;
-        n1Maximo = 50;
-        n2Maximo = 50;
-        break;
-    case 2:
-        probabilidadeSoma = 0.5;
-        probabilidadeSubtracao = 0.5;
-        probabilidadeMultiplicacao = 0.0;
-        transportePermitido = true;
-        n1Maximo = 100;
-        n2Maximo = 100;
-        break;
-    case 3:
-        probabilidadeSoma = 0.25;
-        probabilidadeSubtracao = 0.25;
-        probabilidadeMultiplicacao = 0.5;
-        transportePermitido = true;
-        n1Maximo = 500;
-        n2Maximo = 500;
-        break;
-    /* em JavaScript, default pode estar no meio */
-    default:
-        probabilidadeSoma = (20 - (pontos/10)) >= 0 ? (10 - (pontos/10)) : 5;
-        probabilidadeSubtracao = 3 + (pontos/12);
-        probabilidadeMultiplicacao = 0 + (pontos/10);
-        transportePermitido = true;
-        n1Maximo = (pontos + 1)
-        n2Maximo = (pontos + 1)
-        const normalizador = probabilidadeSoma + probabilidadeSubtracao + probabilidadeMultiplicacao;
-        probabilidadeSoma = probabilidadeSoma / normalizador;
-        probabilidadeSubtracao = probabilidadeSubtracao / normalizador;
-        probabilidadeMultiplicacao = probabilidadeMultiplicacao / normalizador;
-        break;
-  }
-
-  /* define a operação */
-  var probabilidade = Math.random();
-  if(probabilidade < probabilidadeSoma){
-      opsig = "+";
-      op = "Mais";
-  }else if(probabilidade < (probabilidadeSoma + probabilidadeSubtracao)){
-      opsig = "-";
-      op = "Menos"
-  }else if(probabilidade < (probabilidadeSoma + probabilidadeSubtracao + probabilidadeMultiplicacao)){
-      opsig = "x";
-      op = "Vezes";
-  }else{
-      /* jamais deveria chegar nesse ponto, mas tenho que usar essa
-       * verificação por segurança e lançar a exceção adequada */
-      console.log("Problema na escolha de operação na Fábrica de contas: fora das probabilidades de escolha");
-  }
-
-  do{
-      /* define os valores */
-      if(opsig === "+"){
-          n1 = Math.round(Math.random() * n1Maximo);
-          n2 = Math.round(Math.random() * n2Maximo);
-      }else if(opsig === "-"){
-          do{
-              n1 = Math.round(Math.random() * n1Maximo);
-              n2 = Math.round(Math.random() * n2Maximo);
-          /* impede resultados negativos */
-          }while(n1 < n2);
-      }else if(opsig === "x"){
-          /* usa grandezas reduzidas (7% do valor) no caso de multiplicações */
-          n1 = Math.round(Math.random() * n1Maximo * 0.08);
-          n2 = Math.round(Math.random() * n2Maximo * 0.04);
-      }
-
-      /* verifica se há transporte */
-      var houveTransporte = false;
-      var n1Array = n1.toString().split("");
-      var n2Array = n2.toString().split("");
-      /* adiciona zeros no começo dos números até que tenham o mesmo tamanho */
-      while(n1Array.length !== n2Array.length){
-          if(n1Array.length > n2Array.length){
-              n2Array.unshift("0");
-          }
-          if(n1Array.length < n2Array.length){
-              n1Array.unshift("0");
-          }
-      }
-      /* debug */
-      /* alert(transportePermitido);
-      alert(n1);
-      alert(op);
-      alert(n2);
-      alert(n1Array);
-      alert(n2Array); */
-      /* debug */
-      for(var i = 0; i < n1Array.length; i++){
-        switch(opsig){
-          case "+":
-            if((parseInt(n1Array[i], 10) + parseInt(n2Array[i], 10)) > 9){
-              /* debug */
-              /* alert("Transporte detectado em "+parseInt(n1Array[i])+" e "+parseInt(n2Array[i])+"!"); */
-              /* debug */
-              houveTransporte = true;
-            }
-            break;
-          case "-":
-            if((parseInt(n1Array[i], 10) - parseInt(n2Array[i], 10)) < 0){
-              /* debug */
-              /* alert("Transporte detectado em "+parseInt(n1Array[i])+" e "+parseInt(n2Array[i])+"!"); */
-              /* debug */
-              houveTransporte = true;
-            }
-            break;
-          default:
-            break;
-        }
-      }
-  /* repete enquanto não for permitido transporte E houver transporte */
-  }while(!transportePermitido && houveTransporte);
-
-  /* instanciar, atualizar na View e devolver Conta */
-  return {n1: parseInt(n1, 10), n2: parseInt(n2, 10), opsig, op};
 }
---
+++ src/js/Utils.js
@@ -0,0 +1,93 @@
+export class CalcFactory {
+  constructor(){    
+    this.make = this.make.bind(this);
+    this.n1 = null;
+    this.n2 = null;
+    this.op = null;
+    this.opsig = null;
+    this.make = this.make.bind(this);
+  }
+  make(score, level) {
+    this.score = score;
+    this.level = level;
+    const gradSumProb = (20 - (this.score/10)) >= 0 ? (10 - (this.score/10)) : 5;
+    const gradSubProb = 3 + (this.score/12);
+    const gradMultProb = 0 + (this.score/10);
+    const norm = gradSumProb + gradSubProb + gradMultProb;
+    
+    const sumProbs = [0.75, 0.5, 0.25, gradSumProb/norm];
+    const subProbs = [0.25, 0.5, 0.25, gradSubProb/norm];
+    const multProbs = [0.0, 0.0, 0.5 , gradMultProb/norm];
+    const transAllowed = [false, true, true, true];
+    const n1Max = [50, 100, 500, this.score + 1];
+    const n2Max = [50, 100, 500, this.score + 1];
+
+    /* define a operação */
+    const prob = Math.random();    
+    if(prob < sumProbs[this.level - 1]){
+      this.opsig = "+";
+      this.op = "Mais";
+    }else if(prob < (sumProbs[this.level - 1] + subProbs[this.level - 1])){
+      this.opsig = "-";
+      this.op = "Menos"
+    }else if(prob < (sumProbs[this.level - 1] + subProbs[this.level - 1] + multProbs[this.level - 1])){
+      this.opsig = "x";
+      this.op = "Vezes";
+    }else{
+      /* jamais deveria chegar nesse ponto, mas tenho que usar essa
+       * verificação por segurança e lançar a exceção adequada */
+      console.log("Problema na escolha de operação na Fábrica de contas: fora das probabilidades de escolha");
+    }
+
+    do{
+      /* define os valores */
+      if(this.opsig === "+"){
+          this.n1 = Math.round(Math.random() * n1Max[this.level - 1]);
+          this.n2 = Math.round(Math.random() * n2Max[this.level - 1]);
+      }else if(this.opsig === "-"){
+          do{
+              this.n1 = Math.round(Math.random() * n1Max[this.level - 1]);
+              this.n2 = Math.round(Math.random() * n2Max[this.level - 1]);
+          /* impede resultados negativos */
+          }while(this.n1 < this.n2);
+      }else if(this.opsig === "x"){
+          /* usa grandezas reduzidas (7% do valor) no caso de multiplicações */
+          this.n1 = Math.round(Math.random() * n1Max[this.level - 1] * 0.08);
+          this.n2 = Math.round(Math.random() * n2Max[this.level - 1] * 0.04);
+      }
+
+      var transported = false;      
+      var n1Array = this.n1.toString().split("");
+      var n2Array = this.n2.toString().split("");
+      /* adiciona zeros no começo dos números até que tenham o mesmo tamanho */
+      while(n1Array.length !== n2Array.length){
+        if(n1Array.length > n2Array.length){
+            n2Array.unshift("0");
+        }
+        if(n1Array.length < n2Array.length){
+            n1Array.unshift("0");
+        }
+      }      
+      for(var i = 0; i < n1Array.length; i++){
+        switch(this.opsig){
+          case "+":
+            if((parseInt(n1Array[i], 10) + parseInt(n2Array[i], 10)) > 9){              
+              transported = true;
+            }
+            break;
+          case "-":
+            if((parseInt(n1Array[i], 10) - parseInt(n2Array[i], 10)) < 0){              
+              transported = true;
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    /* repete enquanto não for permitido transporte E houver transporte */
+    }while(!transAllowed[this.level - 1] && transported);
+
+    /* instanciar, atualizar na View e devolver Conta */    
+    return {n1: parseInt(this.n1, 10), n2: parseInt(this.n2, 10), opsig: this.opsig, op: this.op};
+  }
+}
